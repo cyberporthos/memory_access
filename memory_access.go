781513ba867a40683577645d21f88d79f0a7dbef
Cyberporthos <cyberporthos@gmail.com>
2016-10-20 20:53:22 -0200

    Adding more options to MEMORY_CLIENT_CHARSET
    Now the allowed values are:
    
        CodePage437
        CodePage850
        CodePage852
        CodePage855
        CodePage858
        CodePage860
        CodePage862
        CodePage863
        CodePage865
        CodePage866
        ISO8859_1
        ISO8859_2
        ISO8859_3
        ISO8859_4
        ISO8859_5
        ISO8859_6
        ISO8859_6E
        ISO8859_6I
        ISO8859_7
        ISO8859_8
        ISO8859_8E
        ISO8859_8I
        ISO8859_10
        ISO8859_13
        ISO8859_14
        ISO8859_15
        ISO8859_16
        KOI8R
        KOI8U
        Macintosh
        MacintoshCyrillic
        Windows874
        Windows1250
        Windows1251
        Windows1252
        Windows1253
        Windows1254
        Windows1255
        Windows1256
        Windows1257
        Windows1258

--- memory_access.go
+++ memory_access.go
@@ -136,14 +136,88 @@ func ChartMap() encoding.Encoding {
     switch ClientCharset() {
     default:
         return nil
+    case "CodePage437":
+        return charmap.CodePage437
+    case "CodePage850":
+        return charmap.CodePage850
+    case "CodePage852":
+        return charmap.CodePage852
+    case "CodePage855":
+        return charmap.CodePage855
+    case "CodePage858":
+        return charmap.CodePage858
+    case "CodePage860":
+        return charmap.CodePage860
+    case "CodePage862":
+        return charmap.CodePage862
+    case "CodePage863":
+        return charmap.CodePage863
+    case "CodePage865":
+        return charmap.CodePage865
+    case "CodePage866":
+        return charmap.CodePage866
     case "ISO8859_1":
         return charmap.ISO8859_1
     case "ISO8859_2":
         return charmap.ISO8859_2
+    case "ISO8859_3":
+        return charmap.ISO8859_3
+    case "ISO8859_4":
+        return charmap.ISO8859_4
+    case "ISO8859_5":
+        return charmap.ISO8859_5
+    case "ISO8859_6":
+        return charmap.ISO8859_6
+    case "ISO8859_6E":
+        return charmap.ISO8859_6E
+    case "ISO8859_6I":
+        return charmap.ISO8859_6I
+    case "ISO8859_7":
+        return charmap.ISO8859_7
+    case "ISO8859_8":
+        return charmap.ISO8859_8
+    case "ISO8859_8E":
+        return charmap.ISO8859_8E
+    case "ISO8859_8I":
+        return charmap.ISO8859_8I
+    case "ISO8859_10":
+        return charmap.ISO8859_10
+    case "ISO8859_13":
+        return charmap.ISO8859_13
+    case "ISO8859_14":
+        return charmap.ISO8859_14
+    case "ISO8859_15":
+        return charmap.ISO8859_15
+    case "ISO8859_16":
+        return charmap.ISO8859_16
+    case "KOI8R":
+        return charmap.KOI8R
+    case "KOI8U":
+        return charmap.KOI8U
+    case "Macintosh":
+        return charmap.Macintosh
+    case "MacintoshCyrillic":
+        return charmap.MacintoshCyrillic
+    case "Windows874":
+        return charmap.Windows874
     case "Windows1250":
         return charmap.Windows1250
+    case "Windows1251":
+        return charmap.Windows1251
     case "Windows1252":
         return charmap.Windows1252
+    case "Windows1253":
+        return charmap.Windows1253
+    case "Windows1254":
+        return charmap.Windows1254
+    case "Windows1255":
+        return charmap.Windows1255
+    case "Windows1256":
+        return charmap.Windows1256
+    case "Windows1257":
+        return charmap.Windows1257
+    case "Windows1258":
+        return charmap.Windows1258
     }
 }
 
